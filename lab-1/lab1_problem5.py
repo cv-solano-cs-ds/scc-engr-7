269f11cff7266ddfb7e7a0d0a9df6d9fa9a0f84e
Cody Vig <cody.cjv@gmail.com>
2023-08-21 09:38:11 -0700

    Added files for lab 1.

---
+++ lab-1/lab1_problem5.py
@@ -0,0 +1,10 @@
+"""
+Helper file for Problem 5 of Lab 1.
+"""
+
+int1: int = int(input("Type an integer: "))
+int2: int = int(input("Type another integer: "))
+
+are_inputs_equal: bool = int1 == int2
+
+print(are_inputs_equal)
