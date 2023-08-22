ff6dc5305203e9f722ec702ae11f7271cb72dbf8
Cody Vig <85211320+CodyVig@users.noreply.github.com>
2023-08-21 23:10:05 -0400

    Lab1 (#1)
    * Added files for lab 1.
    
    * Completed lab 1.

---
+++ lab-1/greet_user.py
@@ -0,0 +1,6 @@
+''' Hello User Script
+Reads in an input string and greets the user
+'''
+
+user_name = input("What is your name?")
+print("Hello, " + user_name + "!")
---
+++ lab-1/lab1_problem5.py
@@ -0,0 +1,13 @@
+"""
+Helper file for Problem 5 of Lab 1.
+"""
+
+int1: int = int(input("Type an integer: "))
+int2: int = int(input("Type another integer: "))
+
+are_inputs_equal: bool = int1 == int2
+
+print(f"{int1=}")
+print(f"{int2=}")
+
+print(f"{are_inputs_equal=}")
